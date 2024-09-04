dfc06694fe423145c78789be1833dd8bf600a364
pragyanshu-kashyap <162783558+pragyanshu-kashyap@users.noreply.github.com>
2024-09-05 00:38:03 +0530

    Add files via upload
    Uploaded landing page

---
+++ landingpage/script.js
@@ -0,0 +1,7 @@
+const startDetectingBtn = document.getElementById('startDetecting');
+
+startDetectingBtn.addEventListener('click', () => {
+  // Handle the button click event
+  // You can implement the image upload and disease detection logic here.
+  console.log("Start Detecting button clicked!");
+});
